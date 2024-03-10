8787200aab2e4c58675d2a90462ef6e30e9bfbf4
Alex Ownejazayeri <alirezaownejazayeri@gmail.com>
2024-03-10 13:12:37 -0700

    learn me a lil' haskell at first

--- problems/p1/haskell/hello.hs
+++ problems/p1/haskell/hello.hs
@@ -1,2 +1,10 @@
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+-- doubleMe :: Num a => a -> a
+-- doubleMe x = x * 2
+
+-- doubleUs :: Num a => a -> a -> a
+-- doubleUs a b = doubleMe a + doubleMe b
+
+-- doubleSmallNumber :: (Ord a, Num a) => a -> a
+-- doubleSmallNumber x = if x > 100
+--                         then x
+--                         else x*2
